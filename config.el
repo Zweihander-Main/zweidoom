a3db4ee36d6fa4488e3c959acde3e2417b19a9af
Zweihänder <zweidev@zweihander.me>
2021-02-04 23:49:21 -0500

    refactor(root): push doom into own dir

---
+++ config.el
@@ -0,0 +1,21 @@
+;;; config.el -- ~/.doom.d/config.el
+;;;
+;;; Commentary:
+;;; -*- lexical-binding: t; -*-
+;;;
+;;; Code:
+
+(load! "+common")
+(load! "+app")
+(load! "+ui")
+(load! "+org")
+(load! "+org-agenda")
+(load! "+org-roam")
+(load! "+mu4e")
+(load! "+code")
+
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
+
+;;; config.el ends here
