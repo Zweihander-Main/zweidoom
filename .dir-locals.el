15813475117daf9e2c598f7626a67d5444917766
Zweihänder <zweidev@zweihander.me>
2021-05-03 22:39:38 -0400

    fix(root): restrict elsa to ondemand
    Elsa has problems with use-package and importing local variables.
    Isn't really worth it -- should split out packages.

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((flycheck-disabled-checkers . (emacs-lisp-elsa)))))
+
+;;; To re-enable on a per-file basis add the following to th end:
+
+;; Local Variables:
+;; flycheck-disabled-checkers: nil
+;; End:
