3cfff10fb711b1ca61cd8df88e3d9a7b57bd0ea2
Zweihänder <zweidev@zweihander.me>
2021-05-22 14:33:56 -0400

    fix(org): change order of goals init

--- org/+org.el
+++ org/+org.el
@@ -94,13 +94,13 @@
 
 (use-package! zweigtd-goals
   :config
+  (setq zweigtd-goals-file zwei/org-agenda-goals-file)
   (zweigtd-goals-init '((:name "1#PHYSICAL"   :key ?1 :color "#CC2200")
                         (:name "2#MENTAL"     :key ?2 :color "#008F40")
                         (:name "3#CODING"     :key ?3 :color "#42A5F5")
                         (:name "4#AUTOMATION" :key ?4 :color "#00FF33")
                         (:name "5#BUSINESS"   :key ?5 :color "#F5C400")
-                        (:name "6#WANKER"     :key ?6 :color "#6A3B9F")))
-  (setq zweigtd-goals-file zwei/org-agenda-goals-file))
+                        (:name "6#WANKER"     :key ?6 :color "#6A3B9F"))))
 
 
 ;; Filing
