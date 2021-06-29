5d9ea8e76ef5f27d72bed6c358c4465ac923b81f
Zweihänder <zweidev@zweihander.me>
2021-06-29 16:18:59 -0400

    feat(+app): integrate with arch trash

--- +app.el
+++ +app.el
@@ -239,4 +239,11 @@
       (setq clipboard (substring clipboard 0 -1))
       (insert clipboard))))
 
+;; System trash
+(when (string= (zwei/which-linux-distro) "Arch")
+  (setq delete-by-moving-to-trash t)
+  (defun system-move-file-to-trash (file)
+    "Send FILE to trash using `trash-put'."
+    (call-process "trash-put" nil nil nil file)))
+
 ;;; +app.el ends here
