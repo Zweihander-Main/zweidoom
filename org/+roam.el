0855579fb2b5915a0579c48f5b720884b8c29050
Zweihänder <zweidev@zweihander.me>
2021-09-04 15:17:53 -0400

    feat(init): add in biblio module

--- org/+roam.el
+++ org/+roam.el
@@ -236,8 +236,6 @@ No API key needed for minor use."
 
 (use-package! org-ref
   :after org-roam
-  :commands (zwei/ref-isbn-from-title
-             zwei/bib+ref+roam-book-title)
   :config
   (let ((bib-files
          (mapcar (lambda (f)
