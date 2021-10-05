5e1a7c9f5b773e75bd27a3f4bce2b685219a5b2e
Zweihänder <zweidev@zweihander.me>
2021-10-04 22:19:01 -0400

    fix(+org): add property toggle

--- org/+org.el
+++ org/+org.el
@@ -17,7 +17,6 @@
          (format "%s --color=never -i -H -E .git -e org --regex"
                  doom-projectile-fd-binary)))
     (doom-project-find-file zwei/org-agenda-directory)))
-  ;;; Archive related
 
 (defun zwei/org-archive-done-tasks ()
   "Archive all done tasks."
@@ -36,11 +35,38 @@
      (string= "TODO" state)
      (string= "NEXT" state))))
 
+(defun zwei/org-hide-properties ()
+  "Hide all org-mode headline property drawers in buffer.
+Could be slow if it has a lot of overlays."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward
+            "^ *:properties:\n\\( *:.+?:.*\n\\)+ *:end:\n" nil t)
+      (let ((ov_this (make-overlay (match-beginning 0) (match-end 0))))
+        (overlay-put ov_this 'display "")
+        (overlay-put ov_this 'hidden-prop-drawer t))))
+  (put 'org-toggle-properties-hide-state 'state 'hidden))
+
+(defun zwei/org-show-properties ()
+  "Show all org-mode property drawers hidden by org-hide-properties."
+  (interactive)
+  (remove-overlays (point-min) (point-max) 'hidden-prop-drawer t)
+  (put 'org-toggle-properties-hide-state 'state 'shown))
+
+(defun zwei/org-toggle-properties ()
+  "Toggle visibility of property drawers."
+  (interactive)
+  (if (eq (get 'org-toggle-properties-hide-state 'state) 'hidden)
+      (org-show-properties)
+    (org-hide-properties)))
+
 ;; Mappings
 (map! :after org
       :map org-mode-map
-      :m "gS" #'org-toggle-pretty-entities
-      :m "gP" #'org-latex-preview
+      :m "gSp" #'zwei/org-toggle-properties
+      :m "gSe" #'org-toggle-pretty-entities
+      :m "gSl" #'org-latex-preview
       :localleader
       :prefix "r"
       :desc "Archive all done tasks" "a" #'zwei/org-archive-done-tasks)
