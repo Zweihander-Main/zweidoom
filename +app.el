fa23ddab3f9b94c7a9a1fc0c91e593bb07b4f724
Zweihänder <zweidev@zweihander.me>
2022-01-17 17:11:45 -0500

    fix(+app): remove old cask code

--- +app.el
+++ +app.el
@@ -15,9 +15,7 @@
 ;; Load lisp files for compiled Emacs on Debian/WSL/Arch
 (when (or (string= (zwei/which-linux-distro) "Debian")
           (string= (zwei/which-linux-distro) "Arch"))
-  (add-to-list 'load-path "/usr/share/emacs/site-lisp")
-  (require 'cask "/usr/share/emacs/site-lisp/cask/cask.el")
-  (cask-initialize doom-private-dir))
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp"))
 
 
 ;; ===============
