dfc6eb32f0da9e8ce6eda8d580f98acc384261b5
Zweihänder <zweidev@zweihander.me>
2022-01-19 21:58:56 -0500

    feat(init): add in docker support

--- init.el
+++ init.el
@@ -76,7 +76,7 @@
        biblio
        debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       (docker +lsp)
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
