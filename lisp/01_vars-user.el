8ff6482fcc2bd7e95e7471458ca8639052d7c026
Zweihänder <zweidev@zweihander.me>
2022-02-09 21:01:26 -0500

    refactor(users): move user vars to own file

---
+++ lisp/01_vars-user.el
@@ -0,0 +1,12 @@
+;;; 01_vars-users --- identity info -*-lexical-binding:t-*-
+;;;
+;;; Commentary:
+;;;
+;;; Identify information.
+;;;
+;;; Code:
+
+(setq user-full-name "Zweihänder"
+      user-mail-address "zweidev@zweihander.me")
+
+;;; 01_vars-user ends here
