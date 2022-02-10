792593158d4ec2756cfa773eb256673ca0bc72e4
Zweihänder <zweidev@zweihander.me>
2022-02-09 21:32:50 -0500

    refactor(docsets): break out docset config

---
+++ lisp/50_browser.el
@@ -0,0 +1,12 @@
+;;; 50_browser --- browsing in emacs -*-lexical-binding:t-*-
+;;;
+;;; Commentary:
+;;;
+;;; `eww' and other browser configuration.
+;;;
+;;; Code:
+
+(after! eww
+  (setq eww-search-prefix "https://lite.duckduckgo.com/lite/?q="))
+
+;;; 50_browser ends here
---
+++ lisp/60_docsets.el
@@ -0,0 +1,11 @@
+;;; 60_docsets --- docsets related -*-lexical-binding:t-*-
+;;;
+;;; Commentary:
+;;;
+;;; Using Dash and similar docsets whenever possible.
+;;;
+;;; Code:
+
+(setq +lookup-open-url-fn #'eww)
+
+;;; 60_docsets ends here
