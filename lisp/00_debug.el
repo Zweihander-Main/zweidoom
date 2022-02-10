1adf6e376061cb22b264b76b33e868847bedb2b1
Zweihänder <zweidev@zweihander.me>
2022-02-09 22:38:04 -0500

    docs(debug): add note about debug procedure

--- lisp/00_debug.el
+++ lisp/00_debug.el
@@ -21,6 +21,7 @@
          (push (float-time (time-since time)) results)))
      (message "%.06f" (/ (apply '+ results) (length results)))))
 
+;; https://github.com/hlissner/doom-emacs/issues/4498
 (when doom-debug-p
   (require 'benchmark-init)
   (add-hook 'doom-first-input-hook #'benchmark-init/deactivate))
