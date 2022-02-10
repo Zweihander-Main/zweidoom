fbfb734e0513e84a95691c91d8bf4a6063207e70
Zweihänder <zweidev@zweihander.me>
2022-02-09 20:58:23 -0500

    refactor(hacks): break out hacks and suppress

---
+++ lisp/00_hacks.el
@@ -0,0 +1,13 @@
+;;; 00_hacks --- emacs hacks -*-lexical-binding:t-*-
+;;;
+;;; Commentary:
+;;;
+;;; Random hacks needed to get Emacs working.
+;;;
+;;; Code:
+
+;; Fix issues with emacs 27 warnings
+;; (setq byte-compile-warnings '(cl-functions))
+;; (setq warning-suppress-log-types '((package reinitialization)))
+
+;;; 00_hacks ends here
