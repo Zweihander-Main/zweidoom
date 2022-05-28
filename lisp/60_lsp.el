d75b899fc0bf5474e41110f8515d9f4725dccbd8
Zweihänder <zweidev@zweihander.me>
2022-05-28 19:49:24 -0400

    feat(lsp): slow down lsp ui

---
+++ lisp/60_lsp.el
@@ -0,0 +1,11 @@
+;;; 60_lsp.el --- general lsp settings -*-lexical-binding:t-*-
+;;;
+;;; Commentary:
+;;;
+;;; LSP settings for all languages.
+;;;
+;;; Code:
+
+(setq lsp-idle-delay 2)
+
+;;; 60_lsp.el ends here
