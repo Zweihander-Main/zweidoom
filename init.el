4b5a89f8582d5e9dca1eab576d4145b7f039586e
Zweihänder <zweidev@zweihander.me>
2022-06-08 23:58:25 -0400

    fix(init): revert eglot

--- init.el
+++ init.el
@@ -76,13 +76,13 @@
        biblio
        debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       (docker)
+       (docker +lsp)
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        (lookup +dictionary +offline +docsets)              ; navigate your code and its documentation
-       (lsp +eglot)                 ; M-x vscode
+       (lsp)                 ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
