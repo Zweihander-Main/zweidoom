d929d24e988c7a9292d095bab26eb70eb99d0784
Zweihänder <zweidev@zweihander.me>
2022-07-06 17:21:17 -0400

    fix(mu4e): force mu4e 1.8 working

--- lisp/50_mu4e.el
+++ lisp/50_mu4e.el
@@ -27,4 +27,25 @@
         mu4e-compose-context-policy nil
         mu4e-org-link-query-in-headers-mode nil))
 
+;; TEMP Fix mu incompat
+(after! mu4e
+  (defalias 'mu4e~start 'mu4e--start)
+  (defalias 'mu4e~stop 'mu4e--stop)
+  (defalias 'mu4e~check-requirements 'mu4e--check-requirements)
+  (defalias 'mu4e~proc-sentinel 'mu4e--server-sentinel)
+  (defalias 'mu4e~proc-start 'mu4e--server-start)
+  (defalias 'mu4e~proc-eat-sexp-from-buf 'mu4e--server-eat-sexp-from-buf)
+  (defalias 'mu4e~pong-handler 'mu4e--pong-handler)
+  (defalias 'mu4e~main-view 'mu4e--main-view)
+  (defalias 'mu4e~main-view-real 'mu4e--main-view-real)
+  (defalias 'mu4e~main-view-real-1 'mu4e--main-view-real-1)
+  (defalias 'mu4e~main-menu 'mu4e--main-menu)
+  (defalias 'mu4e~main-action-str 'mu4e--main-action-str)
+  (defalias 'mu4e~main-redraw-buffer 'mu4e--main-redraw-buffer)
+  (defalias 'mu4e~proc-move 'mu4e--server-move)
+  (defalias 'mu4e~mark-check-target 'mu4e--mark-check-target)
+  (defalias 'mu4e~maildirs-with-query 'mu4e--maildirs-with-query)
+  (defalias 'mu4e~longest-of-maildirs-and-bookmarks 'mu4e--longest-of-maildirs-and-bookmarks)
+  (defalias 'mu4e~update-timer 'mu4e--update-timer)
+  (defalias 'mu4e~proc-remove 'mu4e--server-remove))
 ;;; 50_mu4e.el ends here
