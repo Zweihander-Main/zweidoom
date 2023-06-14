c9c2a06a139b4e2c4c462cfc4e75ac6e2ca1655c
Zweihänder <zweidev@zweihander.me>
2023-06-13 21:28:59 -0400

    fix(backup): autosave more frequently
    Specifically match mental model of how it should work -- as in, work
    like other programs.

--- lisp/10_backup.el
+++ lisp/10_backup.el
@@ -12,6 +12,17 @@
       delete-old-versions t ; delete old versions silently
       version-control t ; number backups
       vc-make-backup-files t ; backup version controlled files
-      auto-save-interval 3)
+      auto-save-interval 20 ; Auto-save every 20 characters typed
+      auto-save-visited-interval 5) ; Auto-save after 5 seconds of idle time
+
+;; Automatic saving direct to files
+(auto-save-visited-mode +1)
+
+;; Save all buffers on focus lost
+(add-hook! '(doom-switch-buffer-hook
+             doom-switch-window-hook)
+  (if (buffer-file-name) (save-some-buffers t))) ; avoid saving when switching to a non-file buffer
+(add-function :after after-focus-change-function
+              (lambda () (save-some-buffers t)))
 
 ;;; 10_backup ends here
