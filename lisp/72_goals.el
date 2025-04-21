2785aa70cfd04528f3c6eb179fb8c0e535bdef0b
Zweihänder <zweidev@zweihander.me>
2025-04-20 23:32:12 -0400

    feat(goals): break up and redo colors

--- lisp/72_goals.el
+++ lisp/72_goals.el
@@ -10,11 +10,14 @@
   :after org
   :config
   (setq zweigtd-goals-file zwei/org-agenda-goals-file)
-  (zweigtd-goals-init '((:name "1#PHYSICAL"   :key ?1 :color "#CC2200")
-                        (:name "2#MENTAL"     :key ?2 :color "#008F40")
-                        (:name "3#CODING"     :key ?3 :color "#42A5F5")
-                        (:name "4#AUTOMATION" :key ?4 :color "#00FF33")
-                        (:name "5#BUSINESS"   :key ?5 :color "#F5C400")
-                        (:name "6#WANKER"     :key ?6 :color "#6A3B9F"))))
+  ;; Using https://materialui.co/colors material+metro palettes
+  (zweigtd-goals-init '((:name "1#PHYSICAL"   :key ?1 :color "#D32F2F")
+                        (:name "2#MENTAL"     :key ?2 :color "#43A047")
+                        (:name "3#EDUCATION"  :key ?3 :color "#F57C00")
+                        (:name "4#AUTOMATION" :key ?4 :color "#64DD17")
+                        (:name "5#PR"         :key ?5 :color "#2196F3")
+                        (:name "6#ENTR"       :key ?6 :color "#FBC02D")
+                        (:name "7#WMARKETING" :key ?7 :color "#9C27B0")
+                        (:name "8#WLIFE"      :key ?8 :color "#825A2C"))))
 
 ;;; 72_goals ends here
